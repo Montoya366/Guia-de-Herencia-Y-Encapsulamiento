f654890f5742d890dc5ddcb616c9fdf0bd66c9e0
Ervin Enmanuel Molina Montoya <168238700+Montoya366@users.noreply.github.com>
2024-05-25 11:32:56 -0600

    Update App.java

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Acerca del carro");
          
-        Auto auto= new Auto("Toyota", "Corrola", 2020, 15000, 5, "Gasolina");
+        Auto auto= new Auto("Toyota", "Corrola", 2020, 15000, 5, "Super");
        
         Camion camion = new Camion("Volvo", "FH", 2019, 75000, 20000, 4);
 
